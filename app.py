bf639f9b7d683cb03c20dca00a091a7fa9487858
NJWanaporn <georgematthewsal@gmail.com>
2025-06-07 12:41:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,6 @@ def submit():
     return "Tack! Dina uppgifter har sparats."
 
 if __name__ == "__main__":
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
